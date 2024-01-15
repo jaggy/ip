48975357c0c67d6ae90719e9b8e42f2503744ddc
Jaggy <jaggy@hey.com>
2024-01-15 12:28:20 +0800

    Finish the front-end setup

---
+++ resources/js/components/ui/content-container/ContentContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type Props = {}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <div class="content-container [ px-8 mx-auto max-w-5xl ]">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped></style>
--- resources/pages/landing.vue
+++ resources/pages/landing.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
 import { Button } from "@/components/ui/button"
+import { ContentContainer } from "@/components/ui/content-container"
 
 const message = "Hello"
 </script>
 
 <template>
-    <div>
-        {{ message }}
+    <div class="[ py-12 ]">
+        <ContentContainer>
+            {{ message }}
 
-        <Button>I'm a button</Button>
+            <Button>I'm a button</Button>
+        </ContentContainer>
     </div>
 </template>
