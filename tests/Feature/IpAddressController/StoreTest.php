04e7ced19769a82a0cf8f3d9167e7c871343d190
Jaggy <jaggy@hey.com>
2024-01-15 13:38:14 +0800

    Add a test for creating an ipv6

--- tests/Feature/IpAddressController/StoreTest.php
+++ tests/Feature/IpAddressController/StoreTest.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\Feature\IpAddressController;
 
+use App\Models\IpAddress;
 use Tests\Fixtures\ActingAsRandomUser;
 
 use function Pest\Laravel\assertDatabaseCount;
 
 uses(ActingAsRandomUser::class);
 
-it('creates an ip4 address', function () {
+it('creates an ip v4 address', function () {
     assertDatabaseCount('ip_addresses', 0);
 
     createIpAddress([
@@ -19,6 +20,26 @@ it('creates an ip4 address', function () {
     ]);
 
     assertDatabaseCount('ip_addresses', 1);
+
+    expect(IpAddress::first())
+        ->label     ->toBe('Cloudflare DNS Resolver')
+        ->ip_address->toBe('1.1.1.1');
+});
+
+it("creates an ip v6 address", function () {
+    assertDatabaseCount('ip_addresses', 0);
+
+    createIpAddress([
+        'label'      => 'IBM IPv6 Example',
+        'ip_address' => '2001:db8:3333:4444:5555:6666:7777:8888',
+    ]);
+
+    assertDatabaseCount('ip_addresses', 1);
+
+
+    expect(IpAddress::first())
+        ->label     ->toBe('IBM IPv6 Example')
+        ->ip_address->toBe('2001:db8:3333:4444:5555:6666:7777:8888');
 });
 
 function createIpAddress(array $attributes = [])
